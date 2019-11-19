2585c13457b9ddd2ab73ce50dda1aaf9e79c3760
StellaShi <stella@apples-MacBook-Pro.local>
2019-11-19 17:47:02 +0200

    add new file

---
+++ hi.py
@@ -0,0 +1,4 @@
+name = input ('please entry ur name:')
+hight = input ('please entry ur hight:')
+weight = input ('please entry ur weight:')
+print('hi',name, 'ur hight is:',hight, 'ur weight is:',weight)
